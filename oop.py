8ad76c10b3210238989a6d0e45ecaeb9a7210c6f
Adyxx <47101483+Adyxx@users.noreply.github.com>
2021-11-04 19:56:23 +0100

    správné vykreslování (oop update)

--- oop.py
+++ oop.py
@@ -4,69 +4,59 @@ import board
 WIDTH = 512
 HEIGHT = WIDTH
 DIMENSIONS = 8
-squareSize = WIDTH // DIMENSIONS
 IMAGES = {}
+squareSize = WIDTH // DIMENSIONS
+gameState = board.GameState()
+
+class Board:  
+    def __init__(self, color1, color2, screen):
+        self.color1 = color1
+        self.color2 = color2
+        self.screen = screen
+
+    def drawGameState(self):
+        boardColors = [p.Color(self.color1), p.Color(self.color2)]
+
+        for r in range(DIMENSIONS):
+            for c in range(DIMENSIONS):
+                color = boardColors[((r + c) % 2)]
+                p.draw.rect(self.screen, color, p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
+                piece = gameState.board[r][c]
+                if piece != "--":
+                    self.screen.blit(IMAGES[piece], p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
+
+class Piece:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
 
 def loadImages():
     pieces = ["wh", "bl", "whk", "blk"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (squareSize, squareSize))
 
-class Board:
-    def __init__(self, color1, color2):
-        self.color1 = color1
-        self.color2 = color2
-
-    def drawBoard(self):
-        colors = [p.Color(self.color1), p.Color(self.color2)]
-        screen = p.display.set_mode((WIDTH, HEIGHT))
-        for r in range(DIMENSIONS):
-            for c in range(DIMENSIONS):
-                color = colors[((r + c) % 2)]
-                p.draw.rect(screen, color, p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
-
-    def drawPieces(self):
-        screen = p.display.set_mode((WIDTH, HEIGHT))
-        gameState = board.GameState()
-        for r in range(DIMENSIONS):
-            for c in range(DIMENSIONS):
-                piece = gameState.board[r][c]
-                if piece != "--":
-                    screen.blit(IMAGES[piece], p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
-
-
-class Pawn:
-    def __init__(self, color):
-        self.color = color
-
-    def moves(self):
-        pass
-
-class King:
-    def __init__(self, color):
-        self.color = color
-
-    def moves(self):
-        pass
-
-
 def main():
     p.init()
     loadImages()
     clock = p.time.Clock()
+    board = Board((100, 100, 100), (200, 200, 200), p.display.set_mode((WIDTH, HEIGHT)))
+    piece = Piece()
+    main.clicks = []
     running = True
-    board = Board((100, 100, 100), (200, 200, 200))
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                pass
 
-        board.drawBoard()
+                piece.move()
+                
+        board.drawGameState()
         p.display.flip()
-
         clock.tick(30)
 
 if __name__ == "__main__":
     main()
+
