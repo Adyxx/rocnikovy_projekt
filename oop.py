fc3b7808a5a1117f6feed65e0007cfeb27734c29
Adyxx <47101483+Adyxx@users.noreply.github.com>
2021-11-04 21:37:41 +0100

    highlight (oop update)

--- oop.py
+++ oop.py
@@ -14,24 +14,37 @@ class Board:
         self.color2 = color2
         self.screen = screen
 
-    def drawGameState(self):
+    def drawBoard(self):
         boardColors = [p.Color(self.color1), p.Color(self.color2)]
 
         for r in range(DIMENSIONS):
             for c in range(DIMENSIONS):
                 color = boardColors[((r + c) % 2)]
                 p.draw.rect(self.screen, color, p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
+
+    def drawPieces(self):
+        for r in range(DIMENSIONS):
+            for c in range(DIMENSIONS):
                 piece = gameState.board[r][c]
                 if piece != "--":
                     self.screen.blit(IMAGES[piece], p.Rect(c * squareSize, r * squareSize, squareSize, squareSize))
+
+    def drawHighlight(self):
+        if main.clicks != []:
+            r = main.clicks[-1][0]
+            c = main.clicks[-1][1]
+            if (gameState.board[main.clicks[0][0]][main.clicks[0][1]] != "--"):
+                s = p.Surface((WIDTH / DIMENSIONS, WIDTH / DIMENSIONS))
+                s.set_alpha(150)  # set opacity
+                s.fill(p.Color('orange'))  # set color
+                self.screen.blit(s, (c * (WIDTH / DIMENSIONS), (r * WIDTH / DIMENSIONS)))
 
 class Piece:
     def __init__(self):
         pass
 
     def move(self):
-        if (gameState.board[p.mouse.get_pos()[1] // squareSize][p.mouse.get_pos()[0] // squareSize] != "--"):
-            main.clicks = [] 
+        main.clicks = [] if (gameState.board[p.mouse.get_pos()[1] // squareSize][p.mouse.get_pos()[0] // squareSize] != "--") else main.clicks
         main.clicks.append((p.mouse.get_pos()[1] // squareSize, p.mouse.get_pos()[0] // squareSize))
         if len(main.clicks) == 2:
             if (gameState.board[main.clicks[0][0]][main.clicks[0][1]] != "--"):
@@ -49,9 +62,9 @@ def main():
     p.init()
     loadImages()
     clock = p.time.Clock()
-    board = Board((100, 100, 100), (200, 200, 200), p.display.set_mode((WIDTH, HEIGHT)))
-    piece = Piece()
     main.clicks = []
+    board = Board((200, 200, 200), (100, 100, 100), p.display.set_mode((WIDTH, HEIGHT)))
+    piece = Piece()
     running = True
     while running:
         for e in p.event.get():
@@ -60,8 +73,10 @@ def main():
             elif e.type == p.MOUSEBUTTONDOWN:
 
                 piece.move()
-                
-        board.drawGameState()
+
+        board.drawBoard()
+        board.drawHighlight() 
+        board.drawPieces() 
         p.display.flip()
         clock.tick(30)
 
