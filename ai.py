0eaf2998f7a7ea8e3aa7f87af1ceb729bfc8aaf8
Libor03 <47101299+Libor03@users.noreply.github.com>
2021-11-24 11:00:31 +0100

    Update ai.py

--- ai.py
+++ ai.py
@@ -3,6 +3,9 @@ import pygame as p
 import board
 import random
 import main as man
+#import mysql.connector
+
+
 
 def eidam(gameState):#main
 
@@ -22,44 +25,65 @@ def whatMove(gameState):
     return gameState
     #man.movePiece(gameState, piece, pos1, pos2, row, col)
 
-def value():
+def bValue(moves, gameState):
+    i=0
+    value = 0
+    bestValue =0
+    for r in range(man.DIMENSIONS):
+        for c in range(man.DIMENSIONS):
+            if gameState.board[r][c] == "bl":
+                while(moves[i] != ";"):
+                    value = 0
+                    row = moves[i]
+                    col = moves[i + 1]
+                    if ((col == 0) or (col == man.DIMENSIONS -1)):
+                        value = value + 4
+                    if ((col == 1) or (col == man.DIMENSIONS -2)):
+                        value = value + 3
+
+                    if (value >= bestValue):
+                        bestMov = []
+                        bestMov.append(row)
+                        bestMov.append(col)
+                        bestMov.append(r)
+                        bestMov.append(c)
+                        bestValue = value
+
+
+                    i=i+2
+                i = i + 1
+
+
+    #database(r,c)
     print("Current value:")
+    return bestMov
+'''
+def database(r,c):
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="eidam",
+        password="cheese",
+        database="cheesecake"
+    )
+    moves = mydb.cursor()
+    moves.execute("CREATE DATABASE mydatabase")
+    sql = "INSERT INTO moves (row, col) VALUES (%s, %s)"
+    vall = (r, c)
+    moves.execute(sql, vall)
+
+    mydb.commit()
+    print("arghhhhh")
+'''
 
 def move(gameState, piece, pos1, pos2, row, col):
 
     print("testAI")
 
 def bestMove(gameState):
-    i =0
-    r_save = 0
-    c_save = 0
-    value = 0
-    bestValue = 0
     bestMov = []
     possibleMoves = man.allPossibleMoves(gameState)
-    for r in range(man.DIMENSIONS):
-        for c in range(man.DIMENSIONS):
-            if gameState.board[r][c] == "bl":
-                while(possibleMoves[i] != ";"):
-                    value =0
-                    row = possibleMoves[i]
-                    col = possibleMoves[i+1]
-                    if (col == 0):
-                        value = value+4
-                    if (col == 1):
-                        value = value + 3
+    bestMov = bValue(possibleMoves, gameState)
 
-
-                    if (value >= bestValue):
-                        bestMov = []
-                        bestMov.append(possibleMoves[i])
-                        bestMov.append(possibleMoves[i+1])
-                        bestValue = value
-                        r_save = r
-                        c_save = c
-
-                    i = i + 2
-                i = i + 1
     gameState.board[bestMov[0]][bestMov[1]] = "bl"
-    gameState.board[r_save][c_save] = "--"
+    gameState.board[bestMov[2]][bestMov[3]] = "--"
     print(bestMov)
